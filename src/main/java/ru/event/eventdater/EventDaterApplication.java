d1b6aad27c0965bae15711c52f08fe56eb9eadd3
Vadsxd <vadim.sannikov.2018@mail.ru>
2025-02-14 14:09:25 +0300

    init

---
+++ src/main/java/ru/event/eventdater/EventDaterApplication.java
@@ -0,0 +1,13 @@
+package ru.event.eventdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/event/eventdater/IventDaterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.event.eventdater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IventDaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
